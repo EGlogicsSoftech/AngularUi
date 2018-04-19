8a7ae8e715bfc549f61dcbe74d8e50d5eda3e57e
Ankit Aggarwal <ankit@eglogics.com>
2018-04-19 21:46:43 +0530

    added ngcordova

--- js/demo.js
+++ js/demo.js
@@ -263,12 +263,48 @@ app.directive('dragMe', ['$drag', function($drag) {
   };
 }]);
 
+
+
 //
 // For this trivial demo we have just a unique MainController
 // for everything
 //
 app.controller('MainController', function($rootScope, $scope) {
 
+  $scope.toastMessage = 'enter a message';
+
+    $scope.center = function (message) {
+      $cordovaToast.show(message, 'long', 'center')
+        .then(function (success) {
+          console.log("center msg displayed");
+        }, function (error) {
+          $scope.msg = error.message;
+        });
+    };
+
+    $scope.top = function (message) {
+
+      $cordovaToast
+        .showShortTop(message)
+        .then(function (success) {
+          console.log("short top displayed ");
+        }, function (error) {
+          $scope.msg = error.message;
+        });
+    };
+
+
+    $scope.bottom = function (message) {
+      $cordovaToast
+        .showLongBottom(message)
+        .then(function (success) {
+          console.log("long bottom displayed");
+        }, function (error) {
+          $scope.msg = error.message;
+        });
+    }
+  	
+  
   $scope.swiped = function(direction) {
     alert('Swiped ' + direction);
   };
