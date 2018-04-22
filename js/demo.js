b6486eb7b66703befa31ab5f645182040e5e9912
Ankit Aggarwal <ankit@eglogics.com>
2018-04-22 19:03:29 +0530

    Scroll Bug Fix

--- js/demo.js
+++ js/demo.js
@@ -10,7 +10,6 @@ var app = angular.module('MobileAngularUiExamples', [
   'ngRoute',
   'mobile-angular-ui',
   'ngCordova',
-  'infinite-scroll',
 
   // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'.
   // This is intended to provide a flexible, integrated and and
